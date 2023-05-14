6c266c2ebeb947a57fc804bbb9a3efcd18520515
Suresh Gv <cracktocode.dev@gmail.com>
2023-05-15 04:56:46 +0530

    class 2

--- server.mjs
+++ server.mjs
@@ -1,17 +1,57 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import { GraphQLBoolean, GraphQLFloat, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
 
 const app = express()
 const port = 8080
 
+const Types = new GraphQLObjectType({
+    name: "Types",
+    fields: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        dev: { type: GraphQLBoolean },
+        price: { type: GraphQLFloat }
+    }
+})
+
 const query = new GraphQLObjectType({
     name: "RootQuery",
-    fields:{
-        hello: { 
+    fields: {
+        hello: {
             type: GraphQLString,
             resolve: () => {
                 return `Hello world`
+            }
+        },
+        types: {
+            type: Types,
+            resolve: () => {
+                return {
+                    name: "Suresh GV",
+                    age: 26,
+                    dev: true,
+                    price: 102.35
+                }
+            }
+        },
+        List: {
+            type: new GraphQLList(Types),
+            resolve: () => {
+                return [
+                    {
+                        name: "Suresh GV",
+                        age: 26,
+                        dev: true,
+                        price: 102.35
+                    },
+                    {
+                        name: "Pradeep K",
+                        age: 25,
+                        dev: false,
+                        price: 50.35
+                    }
+                ]
             }
         }
     }
@@ -21,7 +61,7 @@ const schema = new GraphQLSchema({
     query
 })
 
-app.use('/graphql', graphqlHTTP((req, res) =>({
+app.use('/graphql', graphqlHTTP((req, res) => ({
     schema,
     graphiql: true
 })))
